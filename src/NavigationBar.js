e403dc355ce1c1037d25fdfb503c0dbd37cc762b
BenHardin <bbhardin1@gmail.com>
2025-01-06 13:32:42 -0500

    Marginally improves nav bar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Nav, Navbar, Row, Col} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
 
 export default function NavigationBar() {
 
