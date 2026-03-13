613549f4e13ce8dd55dfbe9442d5e83d9e4ecccc
BenHardin <bbhardin1@gmail.com>
2026-03-13 14:42:16 +0000

    Removes unnecessary code, updates routing to lazy routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route, HashRouter} from "react-router-dom"
-import HomePage from "./HomePage";
+import {Switch, Route, HashRouter} from "react-router-dom"
+import React from "react";
+import {Suspense} from "react";
 import NavigationBar from "./NavigationBar";
-import SkillsPage from "./SkillsPage";
-import CertificationsPage from "./CertificationsPage";
-import ProjectsPage from "./ProjectsPage";
-import VolunteerPage from "./VolunteerPage";
-import HobbiesPage from "./HobbiesPage";
-import ContactPage from "./ContactPage";
-import ResumePage from "./ResumePage";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {useEffect} from "react";
-import PublicationsPage from "./PublicationsPage";
+
+const HomePage = React.lazy(() => import('./HomePage'));
+const ResumePage = React.lazy(() => import('./ResumePage'));
+const SkillsPage = React.lazy(() => import('./SkillsPage'));
+const CertificationsPage = React.lazy(() => import('./CertificationsPage'));
+const ProjectsPage = React.lazy(() => import('./ProjectsPage'));
+const PublicationsPage = React.lazy(() => import('./PublicationsPage'));
+const VolunteerPage = React.lazy(() => import('./VolunteerPage'));
+const HobbiesPage = React.lazy(() => import('./HobbiesPage'));
+const ContactPage = React.lazy(() => import('./ContactPage'));
 
 function App() {
   return (
       <>
         <HashRouter basename="/mysite">
             <NavigationBar/>
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-                {/*<Route exact path="/about" component={HomePage}/>*/}
-                <Route exact path="/resume" component={ResumePage}/>
-                <Route exact path="/skills" component={SkillsPage}/>
-                <Route exact path="/training" component={CertificationsPage}/>
-                <Route exact path="/projects" component={ProjectsPage}/>
-                <Route exact path="/publications" component={PublicationsPage}/>
-                <Route exact path="/volunteer" component={VolunteerPage}/>
-                <Route exact path="/hobbies" component={HobbiesPage}/>
-                <Route exact path="/contact" component={ContactPage}/>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    {/*<Route exact path="/about" component={HomePage}/>*/}
+                    <Route exact path="/resume" component={ResumePage}/>
+                    <Route exact path="/skills" component={SkillsPage}/>
+                    <Route exact path="/training" component={CertificationsPage}/>
+                    <Route exact path="/projects" component={ProjectsPage}/>
+                    <Route exact path="/publications" component={PublicationsPage}/>
+                    <Route exact path="/volunteer" component={VolunteerPage}/>
+                    <Route exact path="/hobbies" component={HobbiesPage}/>
+                    <Route exact path="/contact" component={ContactPage}/>
+                    <Route component={() => <div style={{textAlign:"center"}}>Page not found.</div>} />
+                </Switch>
+            </Suspense>
             <p style={{paddingBottom: "4rem"}}></p>
             <div className='bottomElement' style={{paddingTop: "0.5rem", paddingBottom: "0.5rem", paddingLeft: "1rem", color: "black", backgroundColor: "white", fontWeight: "lighter"}}>
                 Website built by Ben Hardin, using React + GitHub Pages
--- src/ExperiencePage.js
+++
@@ -1,6 +0,0 @@
-
-export default function ExperiencePage() {
-    return (
-        <div></div>
-    );
-}
